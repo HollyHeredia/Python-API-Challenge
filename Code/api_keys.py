69c256c66f96d62d5957bc04a776f6bd3c1949b5
Holly Heredia <hollyheredia@gmail.com>
2021-04-27 19:53:55 -0700

    Adding code update.

--- Code/api_keys.py
+++ Code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "2900f11fd9b7f41e4bdaf5e1d5870fdd"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCemi2RffQlQfXyqGZnAPByHN5dxUHfcjQ"
